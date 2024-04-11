ba4ee2417b81b032e52a8ab777707ee1484f8162
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2024-04-11 12:13:43 -0700

    Update script to work with new version of python

--- backup-plugins.py
+++ backup-plugins.py
@@ -1,10 +1,28 @@
-from distutils.dir_util import copy_tree
-from distutils.file_util import copy_file
+import os
+import shutil
+
+def copy_directory(src_dir, dst_dir):
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+
+    for item in os.listdir(src_dir):
+        src_file = os.path.join(src_dir, item)
+        dst_file = os.path.join(dst_dir, item)
+
+        if os.path.isdir(src_file):
+            copy_directory(src_file, dst_file)
+        else:
+            if os.path.exists(dst_file):
+                if os.path.getmtime(src_file) > os.path.getmtime(dst_file):
+                    shutil.copy2(src_file, dst_file)
+            else:
+                shutil.copy2(src_file, dst_file)
 
 CLOUD = "D:\\OneDrive - Microsoft"
-LOCALC4D = "C:\\Users\\cosorgen\\AppData\\Roaming\\Maxon\\Maxon Cinema 4D 2023_BCDB4759"
-LOCALC4DROOT = "C:\\Program Files\\Maxon Cinema 4D 2023"
-LOCALAE = "C:\\Program Files\\Adobe\\Adobe After Effects 2022\\Support Files"
+LOCALC4D = "C:\\Users\\cosorgen\\AppData\\Roaming\\Maxon\\Maxon Cinema 4D 2024_A5DBFF93"
+LOCALC4DROOT = "C:\\Program Files\\Maxon Cinema 4D 2024"
+LOCALAE = "C:\\Program Files\\Adobe\\Adobe After Effects 2024\\Support Files"
+C4DPYTHONFOLDER = "python311"
 
 print("Starting...")
 
@@ -14,35 +32,36 @@ print("Starting...")
 
 # plugins
 print("Copying local C4D plugins to cloud...")
-copy_tree(src=LOCALC4D+"\\plugins", dst=CLOUD+"\\C4D Plugins Windows", update=1)
+copy_directory(LOCALC4D+"\\plugins", CLOUD+"\\C4D Plugins Windows")
 print("Done.")
 
 print("Copying local AE plugins to cloud...")
-copy_tree(src=LOCALAE+"\\Plug-ins\\User", dst=CLOUD+"\\AE Plugins Windows", update=1)
+copy_directory(LOCALAE+"\\Plug-ins\\User", CLOUD+"\\AE Plugins Windows")
 print("Done.")
 
 # scripts
 print("Copying local C4D scripts to cloud...")
-copy_tree(src=LOCALC4D+"\\library\\scripts", dst=CLOUD+"\\C4D Scripts", update=1)
+copy_directory(LOCALC4D+"\\library\\scripts", CLOUD+"\\C4D Scripts")
 print("Done.")
 
 print("Copying local AE scripts to cloud...")
-copy_tree(src=LOCALAE+"\\Scripts", dst=CLOUD+"\\AE Scripts", update=1)
+copy_directory(LOCALAE+"\\Scripts", CLOUD+"\\AE Scripts")
 print("Done.")
 
 # python libs
 print("Copying local C4D python libs to cloud...")
-copy_tree(src=LOCALC4D+"\\python310\\libs", dst=CLOUD+"\\C4D Python Libs", update=1)
+copy_directory(LOCALC4D+"\\"+C4DPYTHONFOLDER+"\\libs", CLOUD+"\\C4D Python Libs")
 print("Done.")
 
 # xpools
 print("Copying local C4D xpools to cloud...")
-copy_tree(src=LOCALC4D+"\\library\\xgroup", dst=CLOUD+"\\C4D Xpresso Pools", update=1)
+copy_directory(LOCALC4D+"\\library\\xgroup", CLOUD+"\\C4D Xpresso Pools")
 print("Done.")
 
 # new.c4d
 print("Copying local new.c4d to cloud...")
-copy_file(src=LOCALC4DROOT+"\\new.c4d", dst=CLOUD+"\\new.c4d", update=1)
+if os.path.exists(LOCALC4DROOT+"\\new.c4d"):
+    shutil.copy2(LOCALC4DROOT+"\\new.c4d", CLOUD+"\\new.c4d")
 print("Done.")
 
 #
@@ -51,33 +70,34 @@ print("Done.")
 
 # plugins
 print("Copying cloud C4D plugins to local...")
-copy_tree(src=CLOUD+"\\C4D Plugins Windows", dst=LOCALC4D+"\\plugins", update=1)
+copy_directory(CLOUD+"\\C4D Plugins Windows", LOCALC4D+"\\plugins")
 print("Done.")
 
 print("Copying cloud AE plugins to local...")
-copy_tree(src=CLOUD+"\\AE Plugins Windows", dst=LOCALAE+"\\Plug-ins\\User", update=1)
+copy_directory(CLOUD+"\\AE Plugins Windows", LOCALAE+"\\Plug-ins\\User")
 print("Done.")
 
 # scripts
 print("Copying cloud C4D scripts to local...")
-copy_tree(src=CLOUD+"\\C4D Scripts", dst=LOCALC4D+"\\library\\scripts", update=1)
+copy_directory(CLOUD+"\\C4D Scripts", LOCALC4D+"\\library\\scripts")
 print("Done.")
 
 print("Copying cloud AE scripts to local...")
-copy_tree(src=CLOUD+"\\AE Scripts", dst=LOCALAE+"\\Scripts", update=1)
+copy_directory(CLOUD+"\\AE Scripts", LOCALAE+"\\Scripts")
 print("Done.")
 
 # python libs
 print("Copying cloud C4D python libs to local...")
-copy_tree(src=CLOUD+"\\C4D Python Libs", dst=LOCALC4D+"\\python39\\libs", update=1)
+copy_directory(CLOUD+"\\C4D Python Libs", LOCALC4D+"\\"+C4DPYTHONFOLDER+"\\libs")
 print("Done.")
 
 # xpools
 print("Copying cloud C4D xpools to local...")
-copy_tree(src=CLOUD+"\\C4D Xpresso Pools", dst=LOCALC4D+"\\library\\xgroup", update=1)
+copy_directory(CLOUD+"\\C4D Xpresso Pools", LOCALC4D+"\\library\\xgroup")
 print("Done.")
 
 # new.c4d
 print("Copying cloud new.c4d to local...")
-copy_file(src=CLOUD+"\\new.c4d", dst=LOCALC4DROOT+"\\new.c4d", update=1)
+if os.path.exists(CLOUD+"\\new.c4d"):
+    shutil.copy2(CLOUD+"\\new.c4d", LOCALC4DROOT+"\\new.c4d")
 print("Done.")
